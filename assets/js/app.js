87f8aaa9f8e4996d9c321e01a46cc225a97f60f9
Sasha Patsel <sashapatsel@calvisitor-10-105-129-175.calvisitor.1918.berkeley.edu>
2018-06-13 09:51:31 -0700

    hit meetup api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
-$( document ).ready(function() {
-
+$(document).ready(function () {
+//key = 4c766497d613573233d2c692a67393e
+  var queryURL = "https://api.meetup.com/find/groups?access_token=4c766497d613573233d2c692a67393e&zip=94103"
+  $.ajax({
+    url: "https://api.meetup.com/2/cities",
+    method: "GET"
+  }).then(function (data) {
+    console.log(data)
+  })
 });
